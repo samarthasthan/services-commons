b42c4324a3062872fb93041dc783ba866af05450
Samarth Asthan <samarthasthan27@gmail.com>
2025-08-18 23:56:35 +0530

    blog commons

---
+++ gen/js/blogstatus.js
@@ -0,0 +1,25 @@
+// source: blog/v1/blog.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.blog.v1.BlogStatus');
+
+/**
+ * @enum {number}
+ */
+proto.blog.v1.BlogStatus = {
+  BLOG_STATUS_UNSPECIFIED: 0,
+  BLOG_STATUS_DRAFT: 1,
+  BLOG_STATUS_PUBLISHED: 2,
+  BLOG_STATUS_ARCHIVED: 3
+};
+
